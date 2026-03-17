1c2acc42c312b1c89099da3e2b9f852476c44e93
Metron84 <info@metronventures.com>
2026-03-18 03:55:19 +0400

    Delete src directory

--- src/app/api/search/route.ts
+++
@@ -1,8 +0,0 @@
-import { search } from "@/lib/dictionary";
-import { NextRequest } from "next/server";
-
-export async function GET(request: NextRequest) {
-  const q = request.nextUrl.searchParams.get("q") ?? "";
-  const results = await search(q);
-  return Response.json(results);
-}
